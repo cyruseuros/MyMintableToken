fc41a4edec3e3d130c963dd516fce42f46ce624d
Uros Perisic <uros.m.perisic@gmail.com>
2018-08-01 10:00:11 +0200

    Basic mintable token implemented

--- contracts/MyMintableToken.sol
+++ contracts/MyMintableToken.sol
@@ -3,5 +3,18 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 contract MyMintableToken is MintableToken {
-  
+  string public constant name = "MyMintableToken";
+  string public constant symbol = "MMT";
+  uint8 public constant decimals = 18;
+
+  // Arbitrary initial amount to test against
+  uint256 public constant INITIAL_SUPPLY = 1000000;
+
+  constructor() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    // All the tokens go to contract deployer
+    balances[msg.sender] = INITIAL_SUPPLY;
+    // Emit transfer from origin/burn pit
+    emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+  }
 }

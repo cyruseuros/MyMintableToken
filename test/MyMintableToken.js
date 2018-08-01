a31933db3a5f8d985a38aa9fb9b66816f3667139
Uros Perisic <uros.m.perisic@gmail.com>
2018-08-01 15:21:27 +0200

    Failing test 2. Don't know how to fix with await syntax.

--- test/MyMintableToken.js
+++ test/MyMintableToken.js
@@ -1,17 +1,41 @@
 var MyMintableToken = artifacts.require("MyMintableToken.sol");
 
-contract('Transaction test', async function (accounts) {
+contract('Transaction test', async function(accounts) {
     it('should initialize 1000000 tokens to msg.sender', async function() {
         let instance = await MyMintableToken.deployed();
-        let balance = await instance.balanceOf(accounts[0]);
-        assert.equal(balance.toNumber(), 1000000);
+        let balance = await instance.balanceOf.call(accounts[0]);
+        assert.equal(balance.toNumber(), 1000000,
+            'contract improperly initialized');
     });
 
     it('should be able to transfer funds', async function() {
         let instance = await MyMintableToken.deployed();
-        let sendingAccount = accounts[0];
-        let receivingAccount = accounts[1];
+        let fromAccount = accounts[2];
+        let toAccount = accounts[3];
+        let spendingAccount = accounts[4];
 
+        // Transfer some funds to fromAccount
+        // Still don't get how the extra parameters work. What data do they modify?
+        await instance.transfer(fromAccount, 100, {
+            from: accounts[0]
+        });
+        let balanceFromAccount = await instance.balanceOf.call(fromAccount);
+        assert.equal(balanceFromAccount, 100, 'regular transfer failed');
 
+        // Approve spendingAccount
+        // Could I place any account that isn't msg.sender inside {from: foo}?
+        await instance.approve(spendingAccount, 10, {
+            from: accounts[0]
+        });
+
+        // Transfer excessive amount
+        assert.fail(await instance.transferFrom(fromAccount,
+            toAccount,
+            500000, {
+                from: spendingAccount
+            })).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0,
+                'cannot transfer unapproved amount');
+        });
     });
 });
